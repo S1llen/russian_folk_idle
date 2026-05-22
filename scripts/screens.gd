4fc54895a01e0755a14a07f47204808515597e0c
S1llen <ScTiVCr@yandex.ru>
2026-05-22 14:44:47 +0500

    Screens

---
+++ scripts/screens.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _on_screen_transition(screen_name: String) -> void:
+	go_to_screen(screen_name)
+
+func go_to_screen(screen_name: String) -> void:
+	if screen_name == "shop":
+		# что делать
+		
+		pass
+	elif screen_name == "main":
+		# что делать
+		
+		pass
